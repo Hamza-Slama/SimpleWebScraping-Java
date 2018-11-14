7b9b3fb67a6b3a54d6aaa50e12b2dd2ba813e17e
hamzaslama <hamzaslama8@gmail.com>
2018-11-14 16:20:30 +0100

    Simple Tree Level 1 Of Myteck.tn Links.

--- src/main/java/MyScraper.java
+++ src/main/java/MyScraper.java
@@ -1,7 +1,10 @@
+import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import java.io.IOException;
 
 /**
  * Created by Hamza Slama (Hamzeoui) on 11/14/18 .
@@ -12,22 +15,64 @@ public class MyScraper {
     public static String BASE_URL = "https://www.mytek.tn";
     public static void main(String[] args) throws Exception {
         long start = System.currentTimeMillis();
+    int count2 = 0 ;
 
 
-        final Document documents = Jsoup.connect("https://www.mytek.tn").get();
+        try {
+            final Document documents = Jsoup.connect("https://www.mytek.tn")
+                    .userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0")
+                    .referrer("http://www.google.com")
+                    .ignoreHttpErrors(true)
+                    .get();
 //        System.out.println(documents.outerHtml());
-        Elements links = documents.select("a[href]");
-        int count = 0 ;
-        for (Element link : links){
-            if (link.attr("href").contains(BASE_URL)){
-            System.out.println("\n link "+link.attr("href"));
-            System.out.println("Text : "+link.text());
-            count++;
+            Elements links = documents.select("a[href]");
+            int count = 0 ;
+            int i = 0 ;
+            for (Element link : links){
+                if (link.attr("href").contains(BASE_URL)){
+                    System.out.println("--------------------- Site n ° "+i+" ----------------");
+                    System.out.println("\n link "+link.attr("href"));
+                    System.out.println("Text : "+link.text());
+                    final Document document1 = Jsoup.connect(link.attr("href"))
+                            .userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0")
+                            .referrer("http://www.google.com")
+                            .ignoreHttpErrors(true)
+                            .get();
+                    Elements links1 = document1.select("a[href]");
+                    for (Element link1 : links1){
+                        count2++;
+                        if (link1.attr("href").contains(BASE_URL)){
+                            //  System.out.println("\n link "+link1.attr("href"));
+                            //System.out.println("Text : "+link1.text());
+                            count2++;
+
+                        }
+
+                    }
+                    System.out.println("le nombre des link de site n° "+i +" = "+count2);
+                    count+=count2;
+                    count2 = 0 ;
+                    long finish = System.currentTimeMillis();
+                    long timeElapsed = finish - start;
+                    System.out.println(""+timeElapsed+"ms" );
+                    i++;
+                    System.out.println("\n Number of Link = "+count);
+
+                }
+            }
+            System.out.println("\n Number of Link = "+count);
+            long finish = System.currentTimeMillis();
+            long timeElapsed = finish - start;
+            System.out.println(""+timeElapsed+"s" );
+
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        } catch (HttpStatusException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        }
-        System.out.println("\n Number of Link = "+count);
-        long finish = System.currentTimeMillis();
-        long timeElapsed = finish - start;
-        System.out.println(""+timeElapsed+"s" );
+
+
     }
 }
