8a4e08140409f993930acce2ab570279a4b1d3b9
hamzaslama <hamzaslama8@gmail.com>
2018-11-14 14:46:08 +0100

    Initial Commit.

---
+++ src/main/java/MyScraper.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Hamza Slama (Hamzeoui) on 11/14/18 .
+ * Email : hamzaslama8@gmail.com
+ */
+public class MyScraper {
+    //psvm
+    public static void main(String[] args) {
+        
+    }
+}
